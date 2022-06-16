bc7069c197ba3460f52024dc9d053e76c453b1b3
Manolo <manuelgmaroto@gmail.com>
2022-06-16 11:06:53 +0200

    added add-serie and show-top components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddSerieComponent } from './components/add-serie/add-serie.component';
 import { HomeComponent } from './components/home/home.component';
+import { ShowTopComponent } from './components/show-top/show-top.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'add-serie', component: AddSerieComponent},
+  {path: 'show-top', component: ShowTopComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { AddSerieComponent } from './components/add-serie/add-serie.component';
+import { ShowTopComponent } from './components/show-top/show-top.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AddSerieComponent,
+    ShowTopComponent
   ],
   imports: [
     BrowserModule,
